9730e1155d8b9b602a2dc75535cf3f7c574f542a
Onome <onome.aniyeye@royaltyteq.com>
2021-11-06 00:12:57 +0000

    remain calculator

---
+++ firstPYapp.py
@@ -0,0 +1,9 @@
+from math import *
+value1 = input("Enter first number: ")
+value2 = input("Enter second number: ")
+result = int(value1) / int(value2)
+result2 = int(value1) % int(value2)
+if result2 == 0:
+    print(floor(result))
+else:
+    print(str(floor(result))+" with a remainder of " +str(result2))
